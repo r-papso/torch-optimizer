52a01ad05638f1d5d6fa677769db35e9ce8e43e6
r-papso <rastislav.papso@gmail.com>
2022-03-15 12:56:13 +0100

    Bugfix

--- torchcat/pruning/constraint.py
+++ torchcat/pruning/constraint.py
@@ -4,7 +4,7 @@ from typing import Any, Callable
 
 from torch import nn
 
-from .cache import Cache
+from .utils import prune_model
 from .pruner import Pruner
 
 
@@ -33,21 +33,18 @@ class ChannelConstraint(Constraint):
 
         self._pruner = pruner
         self._model = model
-        self._cache = Cache.get_cache(model, pruner)
 
     def feasible(self, solution: Any) -> bool:
-        # model = self._cache.get_cached_model(solution)
-        model_cpy = deepcopy(self._model)
-        model_cpy = self._pruner.prune(model_cpy, solution)
+        model = prune_model(self._model, self._pruner, solution)
         result = True
 
-        for module in model_cpy.modules():
+        for module in model.modules():
             weight = getattr(module, "weight", None)
             if weight is not None and any(dim <= 0 for dim in weight.shape):
                 result = False
                 break
 
-        del model_cpy
+        del model
         return result
 
 
